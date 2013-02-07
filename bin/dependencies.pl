6932d26b2d64e98c6843662f021e8209b7faf452
VarusEz <arthur20249@gmail.com>
2013-02-06 22:10:10 -0500

    update bin/setup.pl

--- bin/dependencies.pl
+++ bin/dependencies.pl
@@ -7,17 +7,14 @@ use warnings;
 my $arg = $ARGV[0];
 
 
-sub install_language_package {
+sub general_package {
 	
 	system ("sudo apt-get update");
 	system ("sudo apt-get -y install language-pack-en-base");
 	system ("sudo /usr/sbin/locale-gen en_IN.UTF-8");
 	system ("sudo /usr/sbin/update-locale LANG=en_IN.UTF-8");
-
-}
-
-sub install_git {
 	system ("sudo apt-get -y install git")
+
 }
 
 sub install_java {
@@ -28,13 +25,18 @@ sub install_sge_master {
 	system ("sudo apt-get -y install gridengine-client gridengine-qmon gridengine-exec gridengine-master");
 }
 
+sub install_sge_compute {
+	system ("sudo apt-get -y install gridengine-client gridengine-exec");
+}
+
 
 if ($arg eq "java") {
 	install_java;
 } elsif ($arg eq "SGE_Master"){
 	install_sge_master;
+} elsif ($arg eq "SGE_Compute"){
+	install_sge_compute;
 }else {
-	install_language_package;
-	install_git;
+	general_package;
 }
 
