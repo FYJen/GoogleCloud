9b52f75cab64110e1356630cde4e7dff86bb4aa4
Fjen <arthur20249@gmail.com>
2013-02-11 18:20:01 -0500

    update mont_ephemeral.pl

--- bin/mount_ephemeral.pl
+++ bin/mount_ephemeral.pl
@@ -6,7 +6,7 @@ use warnings;
 
 sub mount_ephemeral_disk {
 
-	my @DiskList = `ls -al /dev/disk/by-id/google-ephemeral-disk-* | cut -d ' ' --fields=10`;
+	my @DiskList = `ls -al /dev/disk/by-id/google-ephemeral-disk-* | cut -d ' ' --fields=9`;
 	my $Num_Disk = @DiskList;
 	my $disk = "";
 
@@ -15,7 +15,7 @@ sub mount_ephemeral_disk {
 		#trim traling newline characters
 		$DiskList[$i] =~ s/\n+$//;
 
-		system("sudo mkdir -p /mnt/scratch$i/");
+		system("sudo mkdir -p /mnt/scratch$i");
 		system("sudo chmod a+w /mnt/scratch$i");
 		system("sudo /usr/share/google/safe_format_and_mount -m \"mkfs.ext4 -F\" $DiskList[$i] /mnt/scratch$i");
 	}
