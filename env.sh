680fea7946ab1ab1b2c1d93966548c48be891e94
Fjen <arthur20249@gmail.com>
2013-03-25 15:35:05 -0400

    updated env.sh

--- env.sh
+++ env.sh
@@ -11,3 +11,8 @@ GSUTIL_HOME="$DIR/tools/gsutil"
 export PATH=$GSUTIL_HOME/current:${PATH}
 
 export PATH=${DIR}/bin:${PATH}
+
+
+#export ACCESS_KEY=YOUR_GOOGLE_ACCESS_KEY
+#export SECRET_KEY=YOUR_GOOGLE_SECRET_KEY
+#export CONTAINER=<pick-a-name> 
