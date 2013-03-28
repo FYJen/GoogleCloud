b2bb163fbd1c39ef51b57a1f6120511c6ec1169c
Quang Trinh <quang.trinh@gmail.com>
2013-03-28 13:34:07 -0400

    updated docs

--- bin/parallel_command.pl
+++ bin/parallel_command.pl
@@ -8,18 +8,26 @@ use Parallel::ForkManager;
 # we need at least 2 arguments 
 my $num_args = $#ARGV + 1;
 if ($num_args < 2) {
-	usage();
+	print "\n\nNeeds two arguments \n\n";
 	exit(1);
 }
+
+my $CMD = $ARGV[1];
+
+print "\n\nAAAA $CMD\n\n";
 
 my @instanceNames = "qtrinh1000 qtrinh1001 qtrinh1002 qtrinh1003 qtrinh1004";
 my $num_of_nodes = length(@instanceNames);
 my $instancePrefix = "qtrinh";
 my $zone = "us-central1-a";
 
+do_command(@instanceNames, $instancePrefix, $zone, $num_of_nodes);
+
 sub do_command {
 
+	my $array = shift;
 	my @instanceNames = @$array;
+
 	my $instancePrefix = shift;
 	my $zone = shift;
 	my $path = shift;
