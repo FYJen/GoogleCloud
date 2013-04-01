ce2583cbd360a62f1cbbc607c77ef1dd42bca534
Fjen <arthur20249@gmail.com>
2013-04-01 16:55:29 -0400

    updated env.sh

--- env.sh
+++ env.sh
@@ -13,6 +13,10 @@ export PATH=$GSUTIL_HOME/current:${PATH}
 export PATH=${DIR}/bin:${PATH}
 
 
-#export ACCESS_KEY=YOUR_GOOGLE_ACCESS_KEY
-#export SECRET_KEY=YOUR_GOOGLE_SECRET_KEY
-#export CONTAINER=<pick-a-name> 
+
+# The following credentials are prepared for MagFS:
+
+export GC_ACCESS_KEY=YOUR_GOOGLE_ACCESS_KEY
+export GC_SECRET_KEY=YOUR_GOOGLE_SECRET_KEY
+# Below can be a random name (all lower cases)
+export GC_CONTAINER=PICK_A_NAME
